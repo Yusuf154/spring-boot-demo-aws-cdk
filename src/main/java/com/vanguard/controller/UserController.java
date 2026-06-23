bac79f8f20985c982ddf79e7ac64777a9d0c3b93
mohdyu <mohd.yusuf@aa.com>
2026-06-23 13:58:59 +0530

    controller updated

--- src/main/java/com/vanguard/controller/UserController.java
+++ src/main/java/com/vanguard/controller/UserController.java
@@ -8,20 +8,22 @@ import org.springframework.web.bind.annotation.RestController;
 import com.vanguard.model.User;
 
 @RestController
-@RequestMapping("/") 
 public class UserController {
+
     @GetMapping("/api/v1/users/{userId}")
     public User getUser(@PathVariable Long userId) {
-    	System.out.println("user ID = "+userId);
-    	User user = new User(userId, "Arsh");
-        //return new User(userId, "Arsh"); // Returns JSON directly
-    	return user;
+        System.out.println("user ID = " + userId);
+        return new User(userId, "Arsh");
     }
-    
-    @GetMapping("/")
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+	
+	@GetMapping("/")
     public String testing() {
-    	System.out.println("testing");
-    	//return new User(userId, "Arsh"); // Returns JSON directly
-    	return "testing method called perfectly";
+        return "OK";
     }
+	
 }
