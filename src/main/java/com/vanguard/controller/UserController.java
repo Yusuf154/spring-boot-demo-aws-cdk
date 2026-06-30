65ce1ab8bcd3bb156c73677a11c095bcf064a8cb
mohdyu <mohd.yusuf@aa.com>
2026-06-30 18:54:20 +0530

    added logs in the controller

--- src/main/java/com/vanguard/controller/UserController.java
+++ src/main/java/com/vanguard/controller/UserController.java
@@ -2,6 +2,8 @@ package com.vanguard.controller;
 
 import java.net.InetAddress;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +14,9 @@ import com.vanguard.model.User;
 
 @RestController
 public class UserController {
+	
+	  // Create the logger instance
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     /*@GetMapping("/api/v1/users/{userId}")
     public User getUser(@PathVariable Long userId) {
@@ -22,12 +27,14 @@ public class UserController {
 	@GetMapping("/api/v1/users/{userId}")
 	public String getUser(@PathVariable Integer userId) throws Exception {
 	    String instanceId = InetAddress.getLocalHost().getHostName();
+	    logger.info("instanceId= {} ", instanceId);
 	    return "User ID = " + userId + " | Served by: " + instanceId;
 	}
 
 
     @GetMapping("/health")
     public ResponseEntity<String> health() {
+	    logger.info("health method called");
     	return ResponseEntity.ok()
             .header("Content-Type", "text/plain")
             .body("OK");
@@ -36,6 +43,7 @@ public class UserController {
 	
 	@GetMapping("/")
     public ResponseEntity<String> testing() {
+	    logger.info("testing method called");
     	return ResponseEntity.ok()
                 .header("Content-Type", "text/plain")
                 .body("OK");
